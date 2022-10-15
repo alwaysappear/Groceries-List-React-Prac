4320bca6166cdb0402aac7f030c1588492b216f3
Raheem Kawojue <kawojue08@gmail.com>
2022-10-15 06:02:53 +0100

    fixed

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -2,7 +2,8 @@ const apiRequest = async (url='', optionsObj=null, errMsg=null) => {
     try {
         const res = await fetch(url, optionsObj)
         if (!res.ok) throw new Error("Please, reload the site.")
-        return res.json()
+        const data = await res.json()
+        return data
     } catch (err) {
         errMsg = err.message
     } finally {
@@ -10,4 +11,4 @@ const apiRequest = async (url='', optionsObj=null, errMsg=null) => {
     }
 }
 
-export default apiRequest;
+export default apiRequest
